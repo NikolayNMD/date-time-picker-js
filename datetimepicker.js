81682f521bcecbf6687d59c7392deb5e416c71d0
NikolayNMD <136238601+NikolayNMD@users.noreply.github.com>
2024-12-27 16:37:00 +0200

    add OK btn some style fix and mobile env add

--- datetimepicker.js
+++ datetimepicker.js
@@ -6,28 +6,44 @@ document.addEventListener("DOMContentLoaded", function () {
     inline: true,
     locale: "uk",
     onReady: function (selectedDates, dateStr, instance) {
+      instance.input.value = "Нічого не обрано";
       const clearButton = document.createElement("button");
-      clearButton.textContent = "Очистити дату та час";
+      clearButton.textContent = "Очистити";
       clearButton.type = "button";
       clearButton.className = "flatpickr-clear-btn";
-      //   clearButton.style.margin = "10px";
-      //   clearButton.style.padding = "5px 10px";
-      //   clearButton.style.backgroundColor = "#f44336";
-      //   clearButton.style.color = "#fff";
-      //   clearButton.style.border = "none";
-      //   clearButton.style.borderRadius = "5px";
-      //   clearButton.style.cursor = "pointer";
       clearButton.disabled = true;
 
       clearButton.addEventListener("click", function () {
         instance.clear();
+        instance.input.value = "Нічого не обрано";
         clearButton.disabled = true;
       });
 
-      instance.calendarContainer.appendChild(clearButton);
+      const okButton = document.createElement("button");
+      okButton.textContent = "OK";
+      okButton.type = "button";
+      okButton.className = "flatpickr-ok-btn";
+      okButton.disabled = true;
+
+      okButton.addEventListener("click", function () {
+        alert(`Ви обрали: ${instance.input.value}`);
+      });
+
+      const buttonContainer = document.createElement("div");
+      buttonContainer.style.display = "flex";
+      buttonContainer.style.alignItems = "center";
+      buttonContainer.style.justifyContent = "center";
+      buttonContainer.style.gap = "60px";
+      buttonContainer.style.marginTop = "10px";
+
+      buttonContainer.appendChild(clearButton);
+      buttonContainer.appendChild(okButton);
+
+      instance.calendarContainer.appendChild(buttonContainer);
 
       instance.config.onChange.push(function (selectedDates) {
         clearButton.disabled = selectedDates.length === 0;
+        okButton.disabled = selectedDates.length === 0;
       });
     },
   });
